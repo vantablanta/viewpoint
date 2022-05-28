0616398cf4638718eedcbc6c241c2b769bff8470
vanta blanta <vantablanta@gmail.com>
2022-05-28 13:14:02 +0300

    models added with model relationships set, pillow to allow image upload set

--- photoapp/models.py
+++ photoapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=200)
+
+    def __str__(self) :
+        return self.category
+
+class Location(models.Model):
+    place = models.CharField(max_length=200)
+
+    def __str__(self) :
+        return self.place
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=100)
+    image_name = models.CharField(max_length=200)
+    image_description = models.TextField()
+    image_location =  models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    image_category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    uploaded = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        ordering = ['-uploaded', '-updated']
+
+    def __str__(self) :
+        return self.image
