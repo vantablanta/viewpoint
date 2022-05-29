ac84c388d0e23c083348a937363dceb1994ffd1f
vanta blanta <vantablanta@gmail.com>
2022-05-29 21:31:18 +0300

    bug fix attempt. server 500 error on heroku deploy

--- viewpoint/settings.py
+++ viewpoint/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
