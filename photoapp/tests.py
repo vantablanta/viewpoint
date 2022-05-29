ad2e4debe03a047a8bd6dedfa2205c9a0f747b42
vanta blanta <vantablanta@gmail.com>
2022-05-29 16:06:02 +0300

    save method test added and reuslts are OK

--- photoapp/tests.py
+++ photoapp/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+
+from photoapp.views import categories
 from .models import Category, Image, Location
 from django.contrib.auth.models import User
 
@@ -6,13 +8,30 @@ from django.contrib.auth.models import User
 class ModelTests(TestCase):
     # setup
     def setUp(self):
-        self.new_location = Location(place = 'Hungary')
-        self.new_category = Category(category = 'babies')
+        self.new_location = Location(place ='Hungary')
+        self.new_category = Category(category='babies')
         self.new_image_owner = User(username='owner')
+        self.new_image_owner.save()
         self.new_image = Image(owner= self.new_image_owner, image_name='Nice Image', image_description='Beautiful Description', 
             image_location=self.new_location, image_category=self.new_category
         )
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_location, Location))
-        self.assertTrue(isinstance(self.new_category,Category))
+    # def test_instance(self):
+    #     self.assertTrue(isinstance(self.new_location, Location))
+    #     self.assertTrue(isinstance(self.new_category,Category))
+    #     self.assertTrue(isinstance(self.new_image,Image))
+
+    def test_save_method(self):
+        self.new_location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0 )
+
+        self.new_category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0 )
+
+
+        self.new_image.save_image()
+        self.new_image_owner = User(username='owner')
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0 )
