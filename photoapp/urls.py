9f1603a387e5a370efe47fa453611a01d61d60b6
vanta blanta <vantablanta@gmail.com>
2022-05-29 13:11:00 +0300

    images are displayed from db, navigation by image category added

--- photoapp/urls.py
+++ photoapp/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('images/', views.images, name='images'),
+
+
+
+    path('category/<str:pk>/', views.categories, name='category'),
+
+  
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photoapp/views.py
+++ photoapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Image
 
-# Create your views here.
 def home(request):
-
     categories = Category.objects.all()
-    context={'categories': categories}
+    images = Image.objects.all()
+    context={'categories': categories, 'images': images}
     return render(request, 'photoapp/index.html', context)
+
+def categories(request, pk):
+    category_id = Category.objects.get(id=pk)
+    images = Image.objects.filter(image_category=category_id)
+    context = {'category_id':category_id, 'images':images}
+    return render(request, 'photoapp/category.html', context)
+
+
+def images(request):
+    images = Image.objects.all()
+    context = {'images':images}
+    return render(request, 'photoapp/images.html', context )
+
+def search_image(request):
+    context={}
+    return render(request, 'photoapp/search.html', context)
